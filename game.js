769cd6a50616f01b2958ef241d423b143be067ea
James J Potts IV <james@pottsiv.com>
2019-07-25 22:05:06 -0700

    Refactored code and animated buttons

--- game.js
+++ game.js
@@ -1,17 +1,43 @@
+var userClickedPattern = [];
 var gamePattern = [];
-
-gamePattern.push(randomChosenColor);
-
 var buttonColors = ["red", "blue", "green", "yellow"]
 
-var randomChosenColor = buttonColors[nextSequence()];
 
-$("#" + randomChosenColor).fadeOut().fadeIn();
-var buttonSound = new Audio("sounds/" + randomChosenColor + ".mp3");
-buttonSound.play();
+
+$(".btn").click(function(event) {
+  var userChosenColor = event.target.id;
+
+  playSound(userChosenColor);
+
+  animatePress(userChosenColor);
+
+  userClickedPattern.push(userChosenColor);
+})
+
+$(document).keypress(function() {
+  nextSequence();
+})
+
+
 
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 3);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
 
-  return randomNumber;
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  gamePattern.push(randomChosenColor);
+
+  playSound(randomChosenColor);
+}
+
+function playSound(name) {
+  var buttonSound = new Audio("sounds/" + name + ".mp3");
+  buttonSound.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
